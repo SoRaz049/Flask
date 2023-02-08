060c39d05d9667ef0331d6d3939b95376cea2e65
Swaraj Barnwal <swarajbarnwal987@gmailcom>
2023-02-08 09:11:07 +0545

    Solve the issue

--- app.py
+++ app.py
@@ -1,11 +1,25 @@
-from flask import Flask
-app = Flask(__name__)
+
+from flask import Flask, render_template
+app = Flask(__name__, template_folder='templete')
 
 @app.route('/')
 
-def indx():
-    return("HEllo World")
+def index():
+    return render_template("index.html")
+    # return ('Hello World!') 
+
+@app.route('/racing')
+
+def racing():
+    return ("I will race this season!!")
+
+@app.route("/baneshor")
+
+def bane():
+    return "walk"
+
 
 if(__name__ == "__main__"):
-    app.run(debug=True)
+    app.run(debug=True, port=8000)
+
     
