3e3e5d89677be127d395f13eded987ec3a709005
Swaraj Barnwal <swarajbarnwal987@gmailcom>
2023-02-09 09:23:14 +0545

    Added the database sqlite and learned about it

--- app.py
+++ app.py
@@ -1,21 +1,27 @@
-
 from flask import Flask, render_template
+from flask_sqlalchemy import SQLAlchemy
+
+
 app = Flask(__name__, template_folder='templete')
+
+app.config['SQLALCHEMY_DATABASE_URI']= "sqlite:///to_do.db"
+
+db= SQLAlchemy(app)
 
 @app.route('/')
 
-def index():
+def index_page():
     return render_template("index.html")
     # return ('Hello World!') 
 
 @app.route('/racing')
 
-def racing():
+def racing_car():
     return ("I will race this season!!")
 
 @app.route("/baneshor")
 
-def bane():
+def bane_to_college():
     return "walk"
 
 
